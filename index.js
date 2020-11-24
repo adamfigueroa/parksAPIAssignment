5af4d4d006402d5ded75b04e9bddb42eacb87384
Adam Figueroa <admiralfiggins@gmail.com>
2020-11-24 13:37:38 -0800

    returns input values/outputs url/imports json

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 
-function getParks() {
+// using input values, output the proper API link
+function getReturnUrl(parkVal, limit = 10) {
   const api_key = 'Epw3JeowxYcGVTUHQPNBPxyGA72Vg96mkwharQxv'
-  const baseUrl =
+  const baseUrl = 'https://developer.nps.gov/api/v1/parks'
+  // remove the comma in between input
+  let parkValSplit = parkVal.split(',');
+  // setup placeholder to receive statecode string
+  let newParkVal = ''
+  // for loop through the input, add string, trim, then update newParkVal
+  for (let i = 0; i < parkValSplit.length; i++) {
+    newParkVal += `${parkValSplit[i].trim()}%2C%20`
+  }
+  // now put it all together
+  let completeUrl = `${baseUrl}?stateCode=${newParkVal}&limit=${limit}&api_key=${api_key}`
+  console.log(completeUrl);
+  fetch(completeUrl)
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json)
+  })
 
   }
-}
 
+// Capture value of both the state code and the max # of results
 function handleParkSearch() {
-  $('#parks').on('submit', function(event) {
+  $('#submitBtn').click('#submit', function(event) {
     event.preventDefault();
-    let parkVal = $('#park').val()
-    getParks(parkVal)
+    let parkVal = $('#stateEntry').val();
+    let limit = $('#maxResults').val();
+    console.log(parkVal);
+    console.log(limit);
+    getReturnUrl(parkVal, limit)
   })
 }
     
   
 
 function main() {
- 
+  handleParkSearch();
     
 
 
